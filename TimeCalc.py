9fe684aff7ae25b032f3b8a24e9ef8a23c30828b
python3njoyer <100963577+python3njoyer@users.noreply.github.com>
2022-06-23 14:43:03 -0700

    Add files via upload

---
+++ TimeCalc.py
@@ -0,0 +1,50 @@
+import csv
+import datetime as dt
+
+courses = {}
+
+with open('Media_Production_Overview.csv', 'r') as readfile:
+    timesheet = csv.reader(readfile)
+    next(timesheet)
+    for row in timesheet:
+        if row[0] != '':
+            courses[row[0]] = []
+            for columns in range(1,4):
+                courses[row[0]].append(row[columns])
+
+
+def time2Seconds(time):
+    time = time.split(':')
+    h = int(time[0])*3600
+    m = int(time[1])*60
+    return h + m + int(time[2])
+
+
+'''def string2Time(time):
+    time = time.split(':')
+    h = int(time[0])
+    m = int(time[1])
+    s = int(time[2])
+    return dt.timedelta(hours=h, minutes=m, seconds=s)'''
+
+
+def seconds2Time(seconds):
+    h = seconds//3600
+    m = (seconds%3600) // 60
+    s = (seconds%3600) % 60
+    return(str(h) + ":" + str(dt.time(0, m, s))[3:])
+
+
+def sum_times(dict):
+    for code in dict:
+        totalTime = 0  # convert 0 to 0 seconds if using timedelta
+        for times in dict[code]:
+            times = times.split(', ')
+            for time in times:
+                if time != '':
+                    time = time2Seconds(time)
+                    totalTime += time
+        print(seconds2Time(totalTime))
+
+
+sums = sum_times(courses)
